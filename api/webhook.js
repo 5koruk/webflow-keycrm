50be809bd12724938ec4740cef5aabc95ae9fc19
5koruk <artemskoruk@gmail.com>
2025-04-22 19:39:45 +0200

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -6,20 +6,21 @@ export default async function handler(req, res) {
   const { name, email, phone } = req.body;
 
   const lead = {
-    contact: {
-      full_name: name || "Не указано",
-      email: email || "",
-      phone: phone || ""
-    },
-    source: "Webflow"
-  };
+  contact: {
+    full_name: name || "",
+    email: email || "",
+    phone: phone || ""
+  },
+  pipeline_id: 2,
+  source: "Webflow"
+};
 
   try {
     const response = await fetch("https://api.keycrm.app/api/v2/leads", {
       method: "POST",
       headers: {
         "Content-Type": "application/json; charset=utf-8",
-        Authorization: "Bearer ВАШ_API_КЛЮЧ"
+        Authorization: "Bearer MDQwMmMxNzdmNmRkMWNjY2NlZmU3ODhlZGY4ZDhkODk5Zjg1MWE2OQ"
       },
       body: JSON.stringify(lead)
     });
